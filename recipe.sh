183d731caf2d9c8aed173d8a681c9059a615c48a
weic-dev <weicdev@SakaiMacBookAir.local>
2014-08-11 11:54:45 +0900

    spec3: レシピデータファイルを読込んで、データを出力（README.mdへ実行方法を記載）

--- recipe.sh
+++ recipe.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
-# コンソール出力
-echo "オムライス"
+# IFSの値を設定
+IFS_BACKUP=$IFS
+IFS=$'\n'
+
+# ファイル名取得
+RECIPIE_DATA=$1
+
+# レシピファイルからデータを取得(改行毎にデータを取得)し、コンソール出力
+i=0
+for FILE in `cat ./$RECIPIE_DATA`
+do
+  i=`expr $i + 1`
+  echo $FILE
+done
+
+# IFSの値を処理前に戻す
+IFS=$IFS_BACKUP
